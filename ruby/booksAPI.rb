3acd5e23b351b6870b4120f8689fc3add93f2653
Sam McGahan <sam@fishy.ninja>
2017-05-10 01:01:59 -0700

    Added GET example

--- ruby/booksAPI.rb
+++ ruby/booksAPI.rb
@@ -1,3 +1,4 @@
+require 'sinatra'
 require 'sinatra/base'
 require 'JSON'
 
@@ -12,20 +13,40 @@ class Booklistapp < Sinatra::Base
 #----- Books API endpoints
 
 	books = File.read('books.json')
-	books_db =JSON.parse(books)
-
+	books_db = JSON.parse(books)
+	
+	# List of all books
 	get '/books' do
 		books_db.to_s
 	end
+
+	#GET a specific book
+	get '/books/:id' do |id|
+		book = books_db.select {|books_db| books_db["id"] == id.to_i}
+		book.to_s
+	  
+	end
+
+
+	# UPDATE
+	
 
 #----- People API endpoints
 
 	people = File.read('people.json')
 	people_db = JSON.parse(people)
-
+	
+	#List all people
 	get '/people' do
 		people_db.to_s
 	end	
+
+	#GET a specific person
+	get '/people/:id' do |id|
+		person = people_db.select {|people_db| people_db["id"] == id.to_i}
+		person.to_s
+	  
+	end
 
 	run! if app_file == $0
 	
