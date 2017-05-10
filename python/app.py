209cf4e2e373a40471d05677f3969ffa26a634d5
Juliana Arrighi <juliana.arrighi@gmail.com>
2017-05-09 23:14:57 -0700

    add Python setup and sample app

---
+++ python/app.py
@@ -0,0 +1,73 @@
+import json
+from flask import Flask
+from flask import jsonify, request
+
+app = Flask(__name__)
+
+
+# Load the 'databases'
+with open('data/books.json') as json_data:
+    book_db = json.load(json_data)
+
+with open('data/people.json') as json_data:
+    user_db = json.load(json_data)
+
+
+### General Routes
+@app.route("/")
+def hello():
+    resp = "Hello World!"
+    return jsonify(resp)
+
+
+### User Routes
+@app.route("/users", methods=['GET'])
+def get_users():
+    num_users = len(user_db)
+    resp = "There are {} users".format(num_users)
+    return jsonify(resp)
+
+@app.route("/user/<int:user_id>", methods=['GET'])
+def get_user(user_id):
+    resp = "User " + user_id
+    return jsonify(resp)
+
+@app.route("/user/add", methods=['POST'])
+def new_user():
+    resp = "Unable to add user"
+    return jsonify(resp)
+
+
+### Book Routes
+@app.route("/books", methods=['GET'])
+def get_books():
+    resp = "Not implemented"
+    return jsonify(resp)
+
+@app.route("/book/<int:user_id>", methods=['GET'])
+def get_book(book_id):
+    resp = "Book " + user_id
+    return jsonify(resp)
+
+@app.route("/book/add", methods=['POST'])
+def new_book():
+    new_id = len(book_db) + 1
+    data = request.json
+
+    try:
+        new_book = {
+            "id": new_id,
+            "author_first_name": data['author_first_name'],
+            "author_last_name": data['author_last_name'],
+            "title": data['title'],
+            "pages": data['pages']}
+        book_db.append(new_book)
+    except KeyError:
+        resp = "Book not added. Incorrect data format"
+        return jsonify(resp)
+
+    resp = "Book added"
+    return jsonify(resp)
+
+if __name__ == "__main__":
+    app.run()
---
+++ python/setup.md
@@ -0,0 +1,53 @@
+If you already have a python3 environment set up, just use that. 
+
+You can check whether you have python install with 
+
+```
+$ python -v
+$ python3 -v
+```
+
+If you have a Python 2 version and a Python 3 version you will most likely see different versions for those two commands. We'll be using Python 3, although the sample app may work with Python 2 as well.
+
+If you don't have version 3.6 or higher installed...
+
+on a mac: 
+
+```
+brew install python3
+```
+
+or on linux:
+
+```
+apt-get install python3
+```
+
+or on windows is a little more involved:
+https://docs.python.org/3/using/windows.html
+http://www.aaronstannard.com/how-to-setup-a-proper-python-environment-on-windows/
+
+
+Then set up a virtual environment to keep packages separate from any other python projects you may work on 
+
+In the `rest-api-challenge/python/` directory:
+
+```
+$ python3 -m venv env
+$ source env/bin/activate
+(env) $ pip install -r requirements.txt
+```
+
+You will be able to do this challenge with just those libraries and the ones that are part of Python's standard library, but you can install more libraries in this environment with:
+
+```
+$ pip install [library name]
+```
+
+now, you can run the application with 
+
+```
+$ python app.py
+```
+
+visit the root url to see the hello world endpoint
